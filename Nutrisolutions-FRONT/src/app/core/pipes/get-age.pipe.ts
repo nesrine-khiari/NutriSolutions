0f8b859c76e9b06f579cdf05e84fb052586336b2
Nesrine <you@example.com>
2025-01-19 11:57:40 +0100

    Fix

--- Nutrisolutions-FRONT/src/app/core/pipes/get-age.pipe.ts
+++ Nutrisolutions-FRONT/src/app/core/pipes/get-age.pipe.ts
@@ -1,7 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'getAge',
+})
+@Injectable({
+  providedIn: 'root',
 })
 export class GetAgePipe implements PipeTransform {
   transform(birthdate: Date): string {
--- Nutrisolutions-FRONT/src/app/features/profile/profile.module.ts
+++ Nutrisolutions-FRONT/src/app/features/profile/profile.module.ts
@@ -5,10 +5,12 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { HealthInfosComponent } from './health-infos/health-infos.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { GetAgePipe } from 'src/app/core/pipes/get-age.pipe';
 
 @NgModule({
   declarations: [ProfilePageComponent, HealthInfosComponent],
   imports: [CommonModule, SharedModule, MatSliderModule, FormsModule],
-  exports: [ProfilePageComponent,HealthInfosComponent],
+  exports: [ProfilePageComponent, HealthInfosComponent],
 })
 export class ProfileModule {}
